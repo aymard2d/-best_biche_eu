b046153c896344e90e45f81da44a968736edf3b2
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2023-11-30 11:16:41 +0100

    search bar

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,6 +2,16 @@ class AnimalsController < ApplicationController
   before_action :set_animals, only: [:show]
   def index
     @animals = Animal.all
+    if params[:query].present?
+      sql_subquery = <<~SQL
+      animals.name @@ :query
+      OR animals.description @@ :query
+      OR animals.skill @@ :query
+      OR animals.specie @@ :query
+      OR animals.escort_name @@ :query
+      SQL
+      @animals = @animals.where(sql_subquery, query: params[:query])
+    end
   end
 
   def show
