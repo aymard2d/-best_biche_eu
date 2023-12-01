6ddde7d167678e12109e116822ccec567f1e9dcb
Sarah Andre <sarahandrre@gmail.com>
2023-12-01 15:54:35 +0100

    pppp

--- app/controllers/owner/dashboards_controller.rb
+++ app/controllers/owner/dashboards_controller.rb
@@ -2,6 +2,8 @@ class Owner::DashboardsController < ApplicationController
   def show
     @animals = Animal.where(owner: current_user)
     @bookings = Booking.where(user: current_user)
+    @owner = current_user
+    @owner_animals = Animal.where(owner_id: @owner.id) if @owner
 
   end
 end
