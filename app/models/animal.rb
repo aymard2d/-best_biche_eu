3ca147675c05da1d38a71d31d1f7bad7e2c0dbb7
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2023-12-01 10:54:39 +0100

    for animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,6 +2,11 @@ class Animal < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  validates :escort_name, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates :skill, :specie, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :address, :price, presence: :true
   
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
