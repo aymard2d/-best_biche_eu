03d3cf3fbc08bd294995ec8af621cdd79c025222
Yacine Tarezouket <y.tarezouket@gmail.com>
2023-12-01 15:18:01 +0100

    dashbaord

--- app/controllers/owner/dashboards_controller.rb
+++ app/controllers/owner/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class Owner::DashboardsController < ApplicationController
   def show
-    @animals = Animal.where(owner: current_user)
-    @bookings = Booking.where(user: current_user)
-
+    @animals = Animal.all
+    @owner = current_user
+    @owner_animals = Animal.where(owner_id: @owner.id) if @owner
   end
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,5 @@
 class Animal < ApplicationRecord
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many :bookings, dependent: :destroy
 
 
