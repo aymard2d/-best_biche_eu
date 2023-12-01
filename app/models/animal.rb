b109b2884e1f3cd08229801a3641339cb80190d5
Sarah Andre <sarahandrre@gmail.com>
2023-12-01 02:15:36 +0100

    show

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,8 +1,8 @@
 class Animal < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings, dependent: :destroy
-  has_one_attached :photo
-  
+  has_many_attached :photos
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
